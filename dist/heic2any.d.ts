0214fe1918c013dedd6f49ffbae79f2cd922404c
kozo yamagata <tune002@gmail.com>
2021-01-04 17:51:17 +0900

    Build heic2any.js

--- dist/heic2any.d.ts
+++ dist/heic2any.d.ts
@@ -1,10 +1,11 @@
 import "./gifshot";
 import "./libheif";
-declare function heic2any({ blob, toType, quality, gifInterval, multiple, }: {
+declare function heic2any({ blob, toType, quality, gifInterval, multiple, useWorker, }: {
     blob: Blob;
     multiple?: true;
     toType?: string;
     quality?: number;
     gifInterval?: number;
+    useWorker?: boolean;
 }): Promise<Blob | Blob[]>;
 export default heic2any;
